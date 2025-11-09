f6ad54783943afd59b8a2484ffdf302fe6e9a30b
Sérgio Henrique <sergiohenriqueqqq@gmail.com>
2025-11-09 01:12:44 -0300

    corrigindo merge anterior

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import * as NavigationBar from 'expo-navigation-bar';
 import { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext.jsx';
-import { AuthProvider } from './src/context/AuthContext.jsx';
 
 export default function App() {
     useEffect(() => {
